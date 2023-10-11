25033fc03fc0c0b6b69716d01c95eb7474fb552c
RoxerCot <zam_haunter.934@hotmail.com>
2023-10-10 19:58:58 -0600

    First

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,12 +7,19 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC1q91wGu7vKrqtpG6X8jEsiJ_FVuF7HPA",
+
+  authDomain: "accesme-0420.firebaseapp.com",
+
+  databaseURL: "https://accesme-0420.firebaseio.com",
+
+  projectId: "accesme-0420",
+
+  storageBucket: "accesme-0420.appspot.com",
+
+  messagingSenderId: "84016031820",
+
+  appId: "1:84016031820:web:0cd1ee26628019f41f6ce9",
 };
 
 const app = initializeApp(firebaseConfig);
