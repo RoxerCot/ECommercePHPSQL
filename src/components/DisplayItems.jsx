ca4d5971f13994cdce51352ba38b8c536bf52c35
RoxerCot <zam_haunter.934@hotmail.com>
2023-11-08 18:36:46 -0600

    Pagination

--- src/components/DisplayItems.jsx
+++ src/components/DisplayItems.jsx
@@ -1,116 +1,61 @@
 import { Card } from "flowbite-react";
-const DisplayItems = () => {
+import { useEffect, useState } from "react";
+const URL = "http://localhost/BackEnd2/Api.php";
+
+const DisplayItems = ({ props }) => {
+  const { page } = props;
+  const [arrayItems, setArrayItems] = useState(false);
+  var dataDisplay = [];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      var data = new FormData();
+      data.append("METHOD", "GETPHONES");
+      const resp = await fetch(URL, {
+        method: "POST",
+        body: data,
+      });
+      const resp_json = await resp.json();
+      setArrayItems(resp_json);
+    };
+    fetchData();
+
+    return () => {};
+  }, [page]);
+  function ObjectToArray(object, array) {
+    for (var i in object) {
+      array.push(Object.values(object[i]));
+    }
+    console.log(array);
+    return array;
+  }
   return (
-    <div className="flex flex-col mt-8 gap-y-6">
-      <div className="flex flex-row justify-center w-screen h-72 gap-x-8">
-        <Card
-          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-          imgSrc=""
-        >
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              <p>Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</p>
-            </h5>
-          </a>
-          <div className="mb-5 mt-2.5 flex items-center">
-            <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-              <p>5.0</p>
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
-            </span>
+    <div className="flex flex-col mt-8 gap-y-6  ml-12 mr-12">
+      <div className="flex flex-row justify-center w-fit gap-x-8 h-fit">
+        {ObjectToArray(arrayItems, dataDisplay).map((arrayItem) => (
+          <Card
+            key={arrayItem[0]}
+            imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+          >
+            <img src={arrayItem[3]} className="h-32 w-5/6 " />
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                <p>{arrayItem[2]}</p>
+              </h5>
+            </a>
             <a
               className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
               href="#"
             >
-              <p>Add to cart</p>
+              <p>Agragar</p>
             </a>
-          </div>
-        </Card>
-
-        <Card
-          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-          imgSrc=""
-        >
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              <p>Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</p>
-            </h5>
-          </a>
-          <div className="mb-5 mt-2.5 flex items-center">
-            <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-              <p>5.0</p>
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
-            </span>
-            <a
-              className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-              href="#"
-            >
-              <p>Add to cart</p>
-            </a>
-          </div>
-        </Card>
-      </div>
-      <div className="flex flex-row justify-center w-screen h-96 gap-x-8">
-        <Card
-          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-          imgSrc=""
-        >
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              <p>Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</p>
-            </h5>
-          </a>
-          <div className="mb-5 mt-2.5 flex items-center">
-            <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-              <p>5.0</p>
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
-            </span>
-            <a
-              className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-              href="#"
-            >
-              <p>Add to cart</p>
-            </a>
-          </div>
-        </Card>
-
-        <Card
-          imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-          imgSrc=""
-        >
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              <p>Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</p>
-            </h5>
-          </a>
-          <div className="mb-5 mt-2.5 flex items-center">
-            <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-              <p>5.0</p>
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              $599
-            </span>
-            <a
-              className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-              href="#"
-            >
-              <p>Add to cart</p>
-            </a>
-          </div>
-        </Card>
+            <div className="flex items-center justify-between">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                ${Intl.NumberFormat().format(arrayItem[4])}
+              </span>
+            </div>
+          </Card>
+        ))}
       </div>
     </div>
   );
--- src/pages/Tablets.jsx
+++ src/pages/Tablets.jsx
@@ -1,8 +1,24 @@
 import DisplayItems from "../components/DisplayItems";
+import { Pagination } from "flowbite-react";
+import { useState } from "react";
+const pages = 5;
+
 const Tablets = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const onPageChange = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <>
-      <DisplayItems />
+      <DisplayItems props={{ page: currentPage, pages: pages }}></DisplayItems>
+
+      <div className="flex overflow-x-auto sm:justify-center mt-8 mb-10">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={pages}
+          onPageChange={onPageChange}
+        />
+      </div>
     </>
   );
 };
